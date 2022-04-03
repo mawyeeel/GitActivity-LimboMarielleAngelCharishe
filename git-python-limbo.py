18b71be0c4e5531f0556bc8586e713ab20f23342
LimboMarielleAngelCharishe <mawyeeel.school@gmail.com>
2022-04-04 00:33:22 +0800

    My Educational Background added

--- git-python-limbo.py
+++ git-python-limbo.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  3 23:50:07 2022
-
-@author: Angel
-"""
+print("My name is Marielle Angel Charishe M. Limbo")
+print("I was born on May 28, 2002")
+print("I live in Brgy. Bantad, Sitio Bantauyan, Boac, Marinduque")
 
